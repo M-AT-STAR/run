23892ab93e9d7ade07717c17c02d2226aef890e9
M-AT-STAR <772mastersunlimited@gmail.com>
2026-05-26 11:55:03 +0200

    Downgrade version from 1.0.1 to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='M-AT-STAR-Tools',
-    version='1.0.1',
+    version='1.0.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
