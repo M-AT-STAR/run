658bb057921881ca893bbcd36d12ea12e35c4934
M-AT-STAR <772mastersunlimited@gmail.com>
2026-05-26 10:38:59 +0200

    Rename package to M-AT-STAR-Tools

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='m0scan',               # The name people will pip install
-    version='1.0.0',             # Update this number when you make changes
+    name='M-AT-STAR-Tools',
+    version='1.0.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'm0scan=m0scan.main:run',  # This lets users just type 'm0scan' in terminal
+            'm0scan=m0scan.main:run',
         ],
     },
     description='M0scan installation wrapper',
-    author='Your Name',
-    url='https://github.com/yourusername/m0scan', # Your GitHub link goes here
+    author='M-AT-STAR',
+    url='https://github.com/M-AT-STAR/run',
 )
-
